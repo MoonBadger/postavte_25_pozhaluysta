730ce2b71dadc7ecbd2410c0d8db8b4a51818312
MoonBadger <86655118+MoonBadger@users.noreply.github.com>
2023-06-01 21:29:06 +0300

    Delete subway-subwayJDBC directory

--- subway-subwayJDBC/src/main/java/com/example/subway/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.subway.repository;
-
-import com.example.subway.data.model.EmployeeModel;
-
-public interface EmployeeRepository {
-    EmployeeModel findEmployeeModelById(Integer id);
-}
